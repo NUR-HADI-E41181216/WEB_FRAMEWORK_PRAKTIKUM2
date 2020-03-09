348003aa6d60dea6f0b5c29eb3806ef53206d952
NUR-HADI-E41181216 <aryajavadd09@gmail.com>
2020-03-09 16:48:56 +0700

    initial commit
    initial commit

---
+++ Web/application/libraries/malasngoding.php
@@ -0,0 +1,12 @@
+<?php 
+ 
+class Malasngoding{
+ 
+	function nama_saya(){
+		echo "Nama saya adalah malasngoding !";
+	}
+ 
+	function nama_kamu($data){
+		echo "Nama kamu adalah ". $data ." !";
+	}
+}
